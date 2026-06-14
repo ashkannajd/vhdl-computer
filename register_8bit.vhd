95f8c3b377a25aed0311403b3755249d5196d019
Habibullah Nabizada <hbnabizada@gmail.com>
2026-06-14 14:37:10 +0330

    fix: Update register_8bit.vhd

--- register_8bit.vhd
+++ register_8bit.vhd
@@ -1,24 +1,31 @@
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.NUMERIC_STD.ALL;
 
 entity register_8bit is
     Port ( clk    : in  STD_LOGIC;
            reset  : in  STD_LOGIC;
            load   : in  STD_LOGIC;
+           INR    : in  STD_LOGIC;
            d_in   : in  STD_LOGIC_VECTOR (7 downto 0);
            d_out  : out STD_LOGIC_VECTOR (7 downto 0));
 end register_8bit;
 
 architecture Behavioral of register_8bit is
+   signal temp_reg :unsigned(7 downto 0) : = (others => '0');
 begin
     process(clk, reset)
     begin
         if reset = '1' then
-            d_out <= (others => '0');
+            temp_reg <= (others => '0');
+            
         elsif rising_edge(clk) then
             if load = '1' then
-                d_out <= d_in;
+                temp_reg <= unsigned (d_in);
+            elsif INR = '1' then   
+                temp_reg <= temp_reg +1;
             end if;
         end if;
     end process;
+    d_out <= STD_LOGIC_VECTOR (temp_reg);
 end Behavioral;
