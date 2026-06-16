b65374b9ddda3839b2defc79bc4e77b7e717e355
Ashkan <ashkannajdmazhar@aut.ac.ir>
2026-06-16 13:52:16 +0000

    refactor(computer): change the micro-operation ordering
    change the ordering basis of the instructions from the time step (Ti) to instruction type
    add some documentation for better readability

--- src/computer.vhd
+++ src/computer.vhd
@@ -180,184 +180,359 @@ begin
   end process initialization_process;
 
   computer_process : process (clk) is
+
+    variable result : unsigned(ac'length downto 0);
+    variable old_ac : std_logic_vector(ac'range);
+    variable old_e  : std_logic;
+
   begin
 
-    if (rising_edge(clk) and start and not s) then
-      -- set the start flipflop s to 1
-      s <= '1';
-    elsif rising_edge(clk) then
-      -- T0
-      -- ################################################################
-      if (not r and t(0)) then
-        ar <= pc;
-      end if;
-      if (r and t(0)) then
-        ar <= (others => '0');
-        tr <= (tr'high downto addresswidth => '0') & pc(addresswidth - 1 downto 0);
-      end if;
+    if rising_edge(clk) then
+      -- ##################################################################
+      -- START / RUN CONTROL
+      -- ##################################################################
+      if (start = '1' and s = '0') then
+        s <= '1';
+      elsif (s = '1') then
+        -- ################################################################
+        -- FETCH
+        -- ################################################################
+        if (r = '0' and t(0) = '1') then
+          ar <= pc;
+        end if;
 
-      -- T1
-      -- ################################################################
-      if (not r and t(1)) then
-        ir <= mem(ar(addresswidth - 1 downto 0));
-        pc <= std_logic_vector(unsigned(pc) + 1);
-      end if;
-      if (r and t(1)) then
-        mem(ar) <= tr;
-        pc      <= (others => '0');
-      end if;
+        if (r = '0' and t(1) = '1') then
+          ir <= mem(to_integer(unsigned(ar)));
+          pc <= std_logic_vector(unsigned(pc) + 1);
+        end if;
 
-      -- T2
-      -- ################################################################
-      if (not r and t(2)) then
-        i  <= ir(wordwidth - 1);
-        ar <= ir(addresswidth - 1 downto 0);
-      end if;
-      if (r and t(2)) then
-        pc  <= std_logic_vector(unsigned(pc) + 1);
-        ien <= '0';
-        r   <= '0';
-        sc  <= '0';
-      end if;
+        -- ################################################################
+        -- DECODE
+        -- ################################################################
+        if (r = '0' and t(2) = '1') then
+          i  <= ir(wordwidth - 1);
+          ar <= ir(addresswidth - 1 downto 0);
+        end if;
 
-      -- T3
-      -- ################################################################
-      -- D7IT3
-      ----------------------------------
-      if (d(7) and i and t(3)) then
-        sc <= '0';
-      end if;
-      if (d(7) and i and t(3) and ir(11)) then
-        fgi            <= '0';
-        ac(7 downto 0) <= inpr;
-      end if;
-      if (d(7) and i and t(3) and ir(10)) then
-        fgo  <= '0';
-        inpr <= ac(7 downto 0);
-      end if;
-      if (d(7) and i and t(3) and ir(9)) then
-        pc <= std_logic_vector(unsigned(pc) + 1) when fgi;
-      end if;
-      if (d(7) and i and t(3) and ir(8)) then
-        pc <= std_logic_vector(unsigned(pc) + 1) when fgo;
-      end if;
-      if (d(7) and i and t(3) and ir(7)) then
-        ien <= '1';
-      end if;
-      if (d(7) and i and t(3) and ir(6)) then
-        ien <= '0';
-      end if;
-      -- D7I'T3
-      ----------------------------------
-      if (d(7) and i and t(3)) then
-        sc <= (others => '0');
-      end if;
-      if (d(7) and i and t(3) and ir(11)) then
-        ac <= (others => '0');
-      end if;
-      if (d(7) and i and t(3) and ir(10)) then
-        e <= '0';
-      end if;
-      if (d(7) and i and t(3) and ir(9)) then
-        ac <= not ac;
-      end if;
-      if (d(7) and i and t(3) and ir(8)) then
-        e <= not e;
-      end if;
-      if (d(7) and i and t(3) and ir(7)) then
-        e  <= ac(0);
-        ac <= e & ac(ac'high downto 1);
-      end if;
-      if (d(7) and i and t(3) and ir(6)) then
-        e  <= ac(ac'high);
-        ac <= ac(ac'high - 1 downto 0) & e;
-      end if;
-      if (d(7) and i and t(3) and ir(5)) then
-        ac <= std_logic_vector(unsigned(ac) + 1);
-      end if;
-      if (d(7) and i and t(3) and ir(4)) then
-        pc <= std_logic_vector(unsigned(pc) + 1) when not ac(ac'high);
-      end if;
-      if (d(7) and i and t(3) and ir(3)) then
-        pc <= std_logic_vector(unsigned(pc) + 1) when ac(ac'high);
-      end if;
-      if (d(7) and i and t(3) and ir(2)) then
-        pc <= std_logic_vector(unsigned(pc) + 1) when ac = (others => '0');
-      end if;
-      if (d(7) and i and t(3) and ir(1)) then
-        pc <= std_logic_vector(unsigned(pc) + 1) when not e;
-      end if;
-      if (d(7) and i and t(3) and ir(0)) then
-        s <= '0';
-      end if;
-      -- D7'IT3
-      ----------------------------------
-      if (not d(7) and i and t(3)) then
-        ar <= mem(ar);
-      end if;
-      -- D7'I'T3
-      ----------------------------------
-      if (not d(7) and not i and t(3)) then
-      -- do nothing
-      end if;
+        -- ################################################################
+        -- INDIRECT
+        -- D7' I T3 : AR <- M[AR]
+        -- ################################################################
+        if (d(7) = '0' and i = '1' and t(3) = '1') then
+          ar <= mem(to_integer(unsigned(ar)))(addresswidth - 1 downto 0);
+        end if;
 
-      -- T4
-      -- ################################################################
-      if (d(0) and t(4)) then
-        dr <= mem(ar);
-      end if;
-      if (d(1) and t(4)) then
-        dr <= mem(ar);
-      end if;
-      if (d(2) and t(4)) then
-        dr <= mem(ar);
-      end if;
-      if (d(3) and t(4)) then
-        sc      <= (others => '0');
-        mem(ar) <= ac;
-      end if;
-      if (d(4) and t(4)) then
-        sc <= (others => '0');
-        pc <= ar;
-      end if;
-      if (d(5) and t(4)) then
-        mem(ar) <= pc;
-        ar      <= std_logic_vector(unsigned(ar) + 1);
-      end if;
-      if (d(6) and t(4)) then
-        dr <= mem (ar);
-      end if;
+        -- ################################################################
+        -- INTERRUPT REQUEST CHECK
+        -- ################################################################
+        if (t(0) = '0' and t(1) = '0' and t(2) = '0' and
+            ien = '1' and (fgi = '1' or fgo = '1')) then
+          r <= '1';
+        end if;
 
-      -- T5
-      -- ################################################################
-      if (d(0) and t(5)) then
-        ac <= ac and dr;
-        sc <= (others => '0');
-      end if;
-      if (d(1) and t(5)) then
-        result := ('0' & unsigned(ac) + '0' & unsigned(dr));
-        e      <= result(result'high);
-        ac     <= restult(result'high - 1 downto 0);
-        sc     <= (others => '0');
-      end if;
-      if (d(2) and t(5)) then
-        ac <= dr;
-        sc <= (others => '0');
-      end if;
-      if (d(5) and t(5)) then
-        pc <= ar;
-        sc <= (others => '0');
-      end if;
-      if (d(6) and t(5)) then
-        dr <= std_logic_vector(unsigned(dr) + 1);
-      end if;
+        -- ################################################################
+        -- INTERRUPT CYCLE
+        -- ################################################################
+        if (r = '1' and t(0) = '1') then
+          ar <= (others => '0');
+          tr <=
+          (
+            tr'high downto addresswidth => '0'
+          ) &
+            pc(addresswidth - 1 downto 0
+              );
+        end if;
 
-      -- T6
-      -- ################################################################
-      if (d(6) and t(7)) then
-        mem(ar) <= dr;
-        pc      <= std_logic_vector(unsigned(pc) + 1) when dr = (others => '0');
-        sc      <= (others => '0');
+        if (r = '1' and t(1) = '1') then
+          mem(to_integer(unsigned(ar))) <= tr;
+          pc                            <= (others => '0');
+        end if;
+
+        if (r = '1' and t(2) = '1') then
+          pc  <= std_logic_vector(unsigned(pc) + 1);
+          ien <= '0';
+          r   <= '0';
+          sc  <= (others => '0');
+        end if;
+
+        -- ################################################################
+        -- MEMORY REFERENCE INSTRUCTIONS
+        -- ################################################################
+
+        -- ################################################################
+        -- AND
+        -- D0T4: DR <- M[AR]
+        -- D0T5: AC <- AC and DR, SC <- 0
+        -- ################################################################
+        if (d(0) = '1' and t(4) = '1') then
+          dr <= mem(to_integer(unsigned(ar)));
+        end if;
+
+        if (d(0) = '1' and t(5) = '1') then
+          ac <= ac and dr;
+          sc <= (others => '0');
+        end if;
+
+        -- ################################################################
+        -- ADD
+        -- D1T4: DR <- M[AR]
+        -- D1T5: AC <- AC + DR, E <- carry, SC <- 0
+        -- ################################################################
+        if (d(1) = '1' and t(4) = '1') then
+          dr <= mem(to_integer(unsigned(ar)));
+        end if;
+
+        if (d(1) = '1' and t(5) = '1') then
+          result := ('0' & unsigned(ac)) + ('0' & unsigned(dr));
+          e      <= result(result'high);
+          ac     <= std_logic_vector(result(ac'range));
+          sc     <= (others => '0');
+        end if;
+
+        -- ################################################################
+        -- LDA
+        -- D2T4: DR <- M[AR]
+        -- D2T5: AC <- DR, SC <- 0
+        -- ################################################################
+        if (d(2) = '1' and t(4) = '1') then
+          dr <= mem(to_integer(unsigned(ar)));
+        end if;
+
+        if (d(2) = '1' and t(5) = '1') then
+          ac <= dr;
+          sc <= (others => '0');
+        end if;
+
+        -- ################################################################
+        -- STA
+        -- D3T4: M[AR] <- AC, SC <- 0
+        -- ################################################################
+        if (d(3) = '1' and t(4) = '1') then
+          mem(to_integer(unsigned(ar))) <= ac;
+          sc                            <= (others => '0');
+        end if;
+
+        -- ################################################################
+        -- BUN
+        -- D4T4: PC <- AR, SC <- 0
+        -- ################################################################
+        if (d(4) = '1' and t(4) = '1') then
+          pc <= ar;
+          sc <= (others => '0');
+        end if;
+
+        -- ################################################################
+        -- BSA
+        -- D5T4: M[AR] <- PC, AR <- AR + 1
+        -- D5T5: PC <- AR, SC <- 0
+        -- ################################################################
+        if (d(5) = '1' and t(4) = '1') then
+          mem(to_integer(unsigned(ar))) <=
+          (
+            wordwidth - 1 downto addresswidth => '0'
+          ) &
+            pc(addresswidth - 1 downto 0
+              );
+
+          ar <= std_logic_vector(unsigned(ar) + 1);
+        end if;
+
+        if (d(5) = '1' and t(5) = '1') then
+          pc <= ar;
+          sc <= (others => '0');
+        end if;
+
+        -- ################################################################
+        -- ISZ
+        -- D6T4: DR <- M[AR]
+        -- D6T5: DR <- DR + 1
+        -- D6T6: M[AR] <- DR, if DR = 0 then PC <- PC + 1, SC <- 0
+        -- ################################################################
+        if (d(6) = '1' and t(4) = '1') then
+          dr <= mem(to_integer(unsigned(ar)));
+        end if;
+
+        if (d(6) = '1' and t(5) = '1') then
+          dr <= std_logic_vector(unsigned(dr) + 1);
+        end if;
+
+        if (d(6) = '1' and t(6) = '1') then
+          mem(to_integer(unsigned(ar))) <= dr;
+
+          if (dr = (dr'range => '0')) then
+            pc <= std_logic_vector(unsigned(pc) + 1);
+          end if;
+
+          sc <= (others => '0');
+        end if;
+
+        -- ################################################################
+        -- REGISTER REFERENCE INSTRUCTIONS
+        -- D7 I' T3
+        -- ################################################################
+        if (d(7) = '1' and i = '0' and t(3) = '1') then
+          sc <= (others => '0');
+
+          -- ##############################################################
+          -- CLA: AC <- 0
+          -- ##############################################################
+          if (ir(11) = '1') then
+            ac <= (others => '0');
+          end if;
+
+          -- ##############################################################
+          -- CLE: E <- 0
+          -- ##############################################################
+          if (ir(10) = '1') then
+            e <= '0';
+          end if;
+
+          -- ##############################################################
+          -- CMA: AC <- not AC
+          -- ##############################################################
+          if (ir(9) = '1') then
+            ac <= not ac;
+          end if;
+
+          -- ##############################################################
+          -- CME: E <- not E
+          -- ##############################################################
+          if (ir(8) = '1') then
+            e <= not e;
+          end if;
+
+          -- ##############################################################
+          -- CIR: AC and E rotate right
+          -- E <- AC(0)
+          -- AC <- E & AC(high downto 1)
+          -- ##############################################################
+          if (ir(7) = '1') then
+            old_ac := ac;
+            old_e  := e;
+
+            e  <= old_ac(0);
+            ac <= old_e & old_ac(old_ac'high downto 1);
+          end if;
+
+          -- ##############################################################
+          -- CIL: AC and E rotate left
+          -- E <- AC(high)
+          -- AC <- AC(high - 1 downto 0) & E
+          -- ##############################################################
+          if (ir(6) = '1') then
+            old_ac := ac;
+            old_e  := e;
+
+            e  <= old_ac(old_ac'high);
+            ac <= old_ac(old_ac'high - 1 downto 0) & old_e;
+          end if;
+
+          -- ##############################################################
+          -- INC: AC <- AC + 1
+          -- ##############################################################
+          if (ir(5) = '1') then
+            ac <= std_logic_vector(unsigned(ac) + 1);
+          end if;
+
+          -- ##############################################################
+          -- SPA: skip if AC is positive
+          -- ##############################################################
+          if (ir(4) = '1') then
+            if (ac(ac'high) = '0') then
+              pc <= std_logic_vector(unsigned(pc) + 1);
+            end if;
+          end if;
+
+          -- ##############################################################
+          -- SNA: skip if AC is negative
+          -- ##############################################################
+          if (ir(3) = '1') then
+            if (ac(ac'high) = '1') then
+              pc <= std_logic_vector(unsigned(pc) + 1);
+            end if;
+          end if;
+
+          -- ##############################################################
+          -- SZA: skip if AC is zero
+          -- ##############################################################
+          if (ir(2) = '1') then
+            if (ac = (ac'range => '0')) then
+              pc <= std_logic_vector(unsigned(pc) + 1);
+            end if;
+          end if;
+
+          -- ##############################################################
+          -- SZE: skip if E is zero
+          -- ##############################################################
+          if (ir(1) = '1') then
+            if (e = '0') then
+              pc <= std_logic_vector(unsigned(pc) + 1);
+            end if;
+          end if;
+
+          -- ##############################################################
+          -- HLT: S <- 0
+          -- ##############################################################
+          if (ir(0) = '1') then
+            s <= '0';
+          end if;
+        end if;
+
+        -- ################################################################
+        -- INPUT/OUTPUT INSTRUCTIONS
+        -- D7 I T3
+        -- ################################################################
+        if (d(7) = '1' and i = '1' and t(3) = '1') then
+          sc <= (others => '0');
+
+          -- ##############################################################
+          -- INP: AC(7 downto 0) <- INPR, FGI <- 0
+          -- ##############################################################
+          if (ir(11) = '1') then
+            ac(7 downto 0) <= inpr;
+            fgi            <= '0';
+          end if;
+
+          -- ##############################################################
+          -- OUT: OUTR <- AC(7 downto 0), FGO <- 0
+          -- ##############################################################
+          if (ir(10) = '1') then
+            outr <= ac(7 downto 0);
+            fgo  <= '0';
+          end if;
+
+          -- ##############################################################
+          -- SKI: skip if input flag is 1
+          -- ##############################################################
+          if (ir(9) = '1') then
+            if (fgi = '1') then
+              pc <= std_logic_vector(unsigned(pc) + 1);
+            end if;
+          end if;
+
+          -- ##############################################################
+          -- SKO: skip if output flag is 1
+          -- ##############################################################
+          if (ir(8) = '1') then
+            if (fgo = '1') then
+              pc <= std_logic_vector(unsigned(pc) + 1);
+            end if;
+          end if;
+
+          -- ##############################################################
+          -- ION: enable interrupts
+          -- ##############################################################
+          if (ir(7) = '1') then
+            ien <= '1';
+          end if;
+
+          -- ##############################################################
+          -- IOF: disable interrupts
+          -- ##############################################################
+          if (ir(6) = '1') then
+            ien <= '0';
+          end if;
+        end if;
       end if;
     end if;
 
